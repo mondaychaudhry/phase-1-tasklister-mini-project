02cf9010bf117dc08ce635a36838d1a1a211059e
Monday <sobaata@gmail.com>
2021-07-27 08:27:15 -0400

    Completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+const x = document.getElementById('new-task-description')
+const y = document.getElementById("new-task-description").nextElementSibling
+const z = document.getElementById('create-task-form')
+
+z.addEventListener('submit', e => {
+  e.preventDefault()
+  handleList(e.target['task'].value)
+  z.reset ()
+})
+
+function handleList (a) {
+  let b = document.createElement('p')
+  let c = document.createElement('button')
+  c.addEventListener('click', xDelete)
+  c.textContent = 'x'
+  b.textContent = a
+  b.appendChild (c)
+  console.log(b)
+  document.querySelector('#tasks').appendChild (b)
+}
+
+function xDelete(d){
+  d.target.parentNode.remove()
+}
+ 
+
